7c874a3009e7d53dda2cfeb6bd17a12da143b891
Rana Hamdy <104355652+ranahamdy1@users.noreply.github.com>
2024-10-14 00:49:34 +0200

    add paypal

--- lib/Features/checkout/presentation/views/widgets/custom_buttom_bloc_consumer.dart
+++ lib/Features/checkout/presentation/views/widgets/custom_buttom_bloc_consumer.dart
@@ -1,9 +1,15 @@
+import 'dart:developer';
+
+import 'package:checkout_payment_ui/Features/checkout/data/models/amount_model.dart';
+import 'package:checkout_payment_ui/Features/checkout/data/models/item_list_model.dart';
 import 'package:checkout_payment_ui/Features/checkout/data/models/payment_intent_input_model.dart';
 import 'package:checkout_payment_ui/Features/checkout/presentation/views/thank_you_view.dart';
+import 'package:checkout_payment_ui/core/api_keys.dart';
 import 'package:checkout_payment_ui/cubit/stripe_cubit.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
 
 import '../../../../../core/widgets/custom_button.dart';
 
@@ -34,9 +40,66 @@ class CustomButtomBlocConsumer extends StatelessWidget {
       builder: (context, state) {
         return CustomButton(
           onTap: (){
-            // here
-            PaymentIntentInputModel paymentIntentInputModel=PaymentIntentInputModel(amount: '100', currency: 'USD');
-            BlocProvider.of<StripeCubit>(context).makePayment(paymentIntentInputModel: paymentIntentInputModel);
+            // // here stripe
+            // PaymentIntentInputModel paymentIntentInputModel=PaymentIntentInputModel(amount: '100', currency: 'USD');
+            // BlocProvider.of<StripeCubit>(context).makePayment(paymentIntentInputModel: paymentIntentInputModel);
+            //--------------------------------------------------------------------------------------------------------
+
+
+            //paypal
+            var amount = AmountModel(
+            total: "100",
+            currency: "USD",
+            details: Details(
+                shipping:"0",
+                shippingDiscount: 0,
+                subtotal: "100"
+            ),
+            );
+            List<Items> orders = [
+              Items(
+                name: "Apple",
+                quantity: 10,
+                price: "4",
+                currency: "USD",
+              ),
+              Items(
+                name: "Apple",
+                quantity: 12,
+                price: "5",
+                currency: "USD",
+              )
+            ];
+
+            var itemList = ItemListModel(items: orders);
+
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (BuildContext context) => PaypalCheckoutView(
+              sandboxMode: true, //test mode
+              clientId: ApiKeys.clientId,
+              secretKey: ApiKeys.payPalSecretKey,
+              transactions: [
+                {
+                  "amount": amount.toJson(),
+                  "description": "The payment transaction description.",
+                  "item_list": itemList.toJson(),
+                }
+              ],
+              note: "Contact us for any questions on your order.",
+              onSuccess: (Map params) async {
+                log("onSuccess: $params");
+                Navigator.pop(context);
+              },
+              onError: (error) {
+                log("onError: $error");
+                Navigator.pop(context);
+              },
+              onCancel: () {
+                print('cancelled:');
+                Navigator.pop(context);
+              },
+            ),
+          ));
           },
             isLoading: state is StripeLoading? true: false,
             text: 'Continue');
--- lib/core/api_keys.dart
+++ lib/core/api_keys.dart
@@ -1,4 +1,9 @@
 class ApiKeys{
+  //stripe
   static String secretKey="sk_test_51OBtZIAwRB02E63Z2g5exwkVWWegFZjaKoBE0IzEsOP0E8dGGQ9Oh2YxNplQ7KzXRBsN77rFUj1eIYuEdeofnLur00F4toX9lk";
   static String publishableKey="pk_test_51OBtZIAwRB02E63ZRoL24mNb9GpFPZpLDKaxNzKf6eFTHEQmxum2RnZNpFT9UJJOuwpbx8113U1NTZBdWS8Kt4zP00YPgGSOFF";
+
+  //payPal
+  static String clientId = "AdGoz8ZQZPDtFE9goxHvv7nfpStVoJ57HrWspQsGqUlfRIz2eY9yDux-RaM-cOhei-XkPLj5ePHaSUHI";
+  static String payPalSecretKey = "EKQD1jLc8ecU0koT7NOTA2C_m-Zjs2G3MluRjgxrdvQv62Nfu9c6lmcK6Y7it_da1u6krUFOjf41omec";
 }
